3aaf7cbdf826f60bc2fa988cd51a195a25341b5b
Helen <ayten@baysal.com>
2021-01-23 18:35:49 +0300

    C# ta Değer ve Referans Tip

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -22,6 +22,61 @@ namespace DegerVeReferansTipler
             //sayilar1[0] elemanı nedir? 99 referans olduğundan
             //array,class, interface ler referans tiplerdir.
             Console.WriteLine(sayilar1[0]);
+
+            Console.WriteLine("------------------");
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.FirstName = "Helen";
+            person2 = person1;
+            person1.FirstName = "Ayten";
+            Console.WriteLine(person2.FirstName);
+            person2.FirstName = "Derin";
+            Console.WriteLine(person1.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Arzu";
+            customer.CreditCardNumber = "12345";
+
+            Employee employee = new Employee();
+            employee.FirstName = "Eda";
+
+            Person person3 = customer;
+            Console.WriteLine(person3.FirstName);
+            Console.WriteLine(((Customer)person3).CreditCardNumber);
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(employee);
+
+
          }
+    }
+
+    //Base class: Person
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+    class Customer:Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+
+    }
+
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
